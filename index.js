79daebedbd621399d648c27311c4d6eec2843b24
h1lsx <139397050+h1lsx@users.noreply.github.com>
2024-01-01 22:02:04 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
-const url = 'https://json-server--3000.local.webcontainer.io/comments';
+const jsonServer = require('json-server')
+const cors = require('cors')
+const path = require('path')
 
-fetch(url)
-  .then((r) => r.json())
-  .then(
-    (json) =>
-      (document.getElementById('output').innerHTML = JSON.stringify(
-        json,
-        null,
-        2
-      ))
-  );
+const server = jsonServer.create()
+const router = jsonServer.router(path.join(__dirname, 'db.json'))
+const middlewares = jsonServer.defaults()
+
+server.use(cors())
+server.use(jsonServer.bodyParser)
+server.use(middlewares)
+server.use(router)
+
+const PORT = 8000
+
+server.listen(PORT, () => {
+  console.log(`JSON Server is running on http://localhost:${PORT}`)
+})
